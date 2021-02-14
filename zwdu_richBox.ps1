7771e49e4bfa03c9e0c601d4932d249f14b5543e
西红柿大芝麻 <747455334@qq.com>
2021-02-14 23:20:49 +0800

    Update zwdu_richBox.ps1

--- zwdu_richBox.ps1
+++ zwdu_richBox.ps1
@@ -8,19 +8,19 @@ $form = New-Object Windows.Forms.Form -Property @{
 }
 function Add-Controller ([string] $t, [Int] $x, [Int] $y, [Int] $w, [Int] $h, [string] $text) {
     $c = New-Object $t -Property @{
-        Location = New-Object System.Drawing.Point $x, $y
-        Size     = New-Object System.Drawing.Size $w, $h
+        Location = New-Object Drawing.Point $x, $y
+        Size     = New-Object Drawing.Size $w, $h
         Text     = $text
     }
     $form.Controls.Add($c)
     return $c
 }
 
-Add-Controller            Windows.Forms.Label               -x 10   -y 10   -w 150  -h 20   -text '地址'
-$urlPath = Add-Controller Windows.Forms.TextBox             -x 10   -y 30   -w 360  -h 20   -text 'https://www.zwdu.com/book/32934/'
-$chs = Add-Controller System.Windows.Forms.RichTextBox      -x 10   -y 60   -w 450  -h 500
-$btnRun = Add-Controller  Windows.Forms.Button              -x 380  -y 20   -w 80   -h 30   -text '获取目录'
-$txtArea = Add-Controller Windows.Forms.RichTextBox         -x 500  -y 10   -w 500  -h 560  -text '正文'
+Add-Controller            Windows.Forms.Label        -x 10   -y 10   -w 150  -h 20   -text '地址'
+$urlPath = Add-Controller Windows.Forms.TextBox      -x 10   -y 30   -w 360  -h 20   -text 'https://www.zwdu.com/book/32934/'
+$chs = Add-Controller     Windows.Forms.RichTextBox  -x 10   -y 60   -w 450  -h 500
+$btnRun = Add-Controller  Windows.Forms.Button       -x 380  -y 20   -w 80   -h 30   -text '获取目录'
+$txtArea = Add-Controller Windows.Forms.RichTextBox  -x 500  -y 10   -w 500  -h 560  -text '正文'
 
 $btnRun.Add_Click(
     {
@@ -29,7 +29,7 @@ $btnRun.Add_Click(
         $html = [system.Text.Encoding]::GetEncoding("gbk").GetString($resp.RawContentStream.ToArray())
         $txtArea.Text = $html
         $block = [regex]::Match($html, 'id="list"[\s\S]*?div')
-        $chs.Text = ([regex]::Matches($block.Value, 'href="(.*?)">(.*?)<') |  ForEach-Object {
+        $chs.Text = ([regex]::Matches($block.Value, 'href="(.*?)">(.*?)<') | ForEach-Object {
                 return "https://www.zwdu.com" + $_.Groups[1].Value + " " + $_.Groups[2].Value
             }) -Join "`n"
     })
